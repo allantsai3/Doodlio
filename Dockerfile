0afa02095c77c2fc448c9a36d33466d108d78a36
Allan <allantsai3@gmail.com>
2020-10-13 20:23:07 -0700

    socket.io test

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,10 @@ ARG NODE_ENV
 ENV NODE_ENV $NODE_ENV
 
 COPY package.json /usr/src/app/
-RUN npm install
+RUN yarn install
 
 COPY . /usr/src/app
 
 ENV PORT 5000
 EXPOSE $PORT
-CMD [ "npm", "start" ]
+CMD [ "yarn", "start" ]
--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('App', function() {
-  it('has the default page', function(done) {
-    request(app)
-      .get('/')
-      .expect(/Welcome to Express/, done);
-  });
+  // it('has the default page', function(done) {
+  //   request(app)
+  //     .get('/')
+  //     .expect(/Welcome to Express/, done);
+  // });
 }); 
